32f6f8dde035dc5c6bd74374c023c46b4bce4d51
twishapatel4 <twisha@techeniac.com>
2026-04-07 12:58:14 +0530

    Project Setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ecopredict',
+    version='0.0.1',
+    packages=find_packages(), # This find all folders with __init__.py (like 'src')
+    install_requires=[]
+)
